3ecda091d762000ccb93445854f5977c4d50263f
MochiBingo <mochibingoshep@gmail.com>
2025-01-06 15:01:39 -0400

    scripts all set up

---
+++ Assets/Actions.cs
@@ -0,0 +1,10 @@
+using System;
+
+public static class Actions
+{
+    public static Action interact;
+    public static Action shoot;
+    public static Action reload;
+    public static Action detonate;
+    public static Action toggleFlashlight;
+}
